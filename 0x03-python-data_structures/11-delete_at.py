1278af9b84a76b25c066a1e82771063f4a8552b2
Ruglawy <git@github.com>
2023-08-12 11:03:16 -0700

    Delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,10 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = []
-    for i in range(len(my_list)):
-        if i == idx:
-            continue
-        new_list.append(my_list[i])
-    my_list = my_list[:idx] + my_list[idx+1:]
-    return new_list
+    del my_list[idx]
+    return my_list

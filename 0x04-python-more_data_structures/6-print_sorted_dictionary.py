b494875862cad95258cfda250f16a158bec35a06
Ruglawy <git@github.com>
2023-08-16 09:46:26 -0700

    PrintSortDict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    b_dictionary = sorted(a_dictionary)
+    for i in b_dictionary:
+        print("{}: {}".format(i, a_dictionary.get(i)))

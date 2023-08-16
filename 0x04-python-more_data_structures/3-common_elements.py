8fb984de46d69379ceabe048afd0eb8e72704b62
Ruglawy <git@github.com>
2023-08-16 09:26:54 -0700

    CommonElements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = list(map(lambda x, y: x if x == y else pass, set_1, set_2))
-    return set(set_3)
+    new_set = set()
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                new_set.add(i)
+    return new_set

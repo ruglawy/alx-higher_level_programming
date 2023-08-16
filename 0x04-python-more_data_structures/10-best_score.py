fe42aa2502c115392cf7dfb22e36b16102d1049c
Ruglawy <git@github.com>
2023-08-16 11:16:29 -0700

    BestScore

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    keylist = list(a_dictionary)
+    if len(keylist) == 0:
+        return None
+    max_num = a_dictionary[keylist[0]]
+    for key in keylist:
+        if a_dictionary[key] > max_num:
+            max_num = a_dictionary[key]

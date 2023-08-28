dc44417f4efee5cd3aace74e469b4db1fb07770c
Ruglawy <git@github.com>
2023-08-28 10:35:23 -0700

    SafePrintINt

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception:", e)
+        sys.stderr.write("Exception:", e)
         return False

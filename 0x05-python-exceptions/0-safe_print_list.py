5566031137adf0f33384f3e995ec7259b9e60b77
Ruglawy <git@github.com>
2023-08-28 09:58:04 -0700

    Safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
+            counter += 1
     except Exception as e:
         pass
     finally:
         print()
+        return counter

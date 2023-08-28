d1568c26aea2235148e2478e7b187f7c56d34d2e
Ruglawy <git@github.com>
2023-08-28 10:41:20 -0700

    SafeFun

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    
+    result = None
+    try:
+        result = fct(*args)
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+    finally:
+        return result

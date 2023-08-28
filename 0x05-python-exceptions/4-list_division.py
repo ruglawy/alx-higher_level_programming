26cf18078970734ea3f8e1d45d596c6e278e48b8
Ruglawy <git@github.com>
2023-08-28 10:23:29 -0700

    ListDiv

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0] * list_length
+    try:
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                new_list[i] = result
+            except ValueError as v:
+                print("wrong type")
+            except ZeroDivisionError as z:
+                print("division by 0")
+            except IndexError as ind:
+                print("out of range")
+    except Exception as e:
+        print("out of range")
+    finally:
+        return new_list

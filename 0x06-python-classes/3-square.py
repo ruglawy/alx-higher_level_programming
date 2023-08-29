f566b82afca2c2bd60e1389dc2ffd9f96eaa4c74
Ruglawy <git@github.com>
2023-08-29 15:38:39 -0700

    SquareSet

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """initialize square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """initialize square
         Args:
             size (int): size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+
+    @property
+    def size(self):
+        """idk
+
+        Return:
+            size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets new size
+        Args:
+            value (int): size of square
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size

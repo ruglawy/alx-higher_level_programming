b0de090abf7cce1f2ef58e1b084fe84de7ca178e
Ruglawy <git@github.com>
2023-08-29 15:40:27 -0700

    SquareSrtter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,13 +9,7 @@ class Square:
         Args:
             size (int): size
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
-
+        self.size = size
 
     @property
     def size(self):

6cf95ffad2107270bc8f28128929e2c1eb58a6b4
Ruglawy <git@github.com>
2023-08-29 15:03:51 -0700

    Swuareeeeeee

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,13 @@
 
 
 class Square:
+    """Class zeft"""
     __size = None
 
     def __init__(self, size = None):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
         if size is not None:
             self.__size = size

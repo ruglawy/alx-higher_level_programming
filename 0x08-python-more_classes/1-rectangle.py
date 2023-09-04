e5c8a17027387b16352e6e2191e2dc979d3ad7ca
Ruglawy <git@github.com>
2023-09-04 13:39:23 -0700

    RecClass

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set value"""
-        if type(value is not int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")

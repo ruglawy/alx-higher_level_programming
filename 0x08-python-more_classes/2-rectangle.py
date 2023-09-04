43a4abffc8626dc82c8d0494a68684a7e78f5fd2
Ruglawy <git@github.com>
2023-09-04 13:47:42 -0700

    RecClassArea

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+class ...
+"""
+
+
+class Rectangle:
+    """ Rectangle class """
+    def __init__(self, width=0, height=0):
+        """constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """return value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set value"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """return value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculate area"""
+        return height * width
+
+    def perimeter(self):
+        """calculate perimeter"""
+        if height == 0 or width == 0:
+            return 0
+        return 2 * (height + width)

ffc58eb10a70acdacbf74d5b67521f6da3d19d65
Ruglawy <git@github.com>
2023-09-04 14:21:56 -0700

    PrintSybmol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         shape = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                shape += str(print_symbol)
+                shape += str(self.print_symbol)
             if i < self.__height - 1:
                 shape += "\n"
         return shape

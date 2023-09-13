618c5954a34f03f19edcad300cc25b0dcbda96e7
Ruglawy <git@github.com>
2023-09-12 17:27:06 -0700

    classtoJson

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" func """
+import json
+
+
+def class_to_json(obj):
+    """ func """
+    result = {}
+    if hasattr(obj, "__dict__"):
+        result = obj.__dict__.copy()
+    return result
